d37981dc709cdc95a3492e12a3caa99dc1a677ba
deuchebaggalo@gmail.com <deuchebaggalo@gmail.com@79cf5116-dcd4-c039-58dd-6f4206d1403f>
2011-02-15 21:35:42 +0000

    Created project

---
+++ Networks/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: genacje
+ *
+ * Created on February 15, 2011, 3:34 PM
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
