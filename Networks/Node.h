2dc834e5f5bf6fcabc252b023f5d1db95b5aa137
deuchebaggalo@gmail.com <deuchebaggalo@gmail.com@79cf5116-dcd4-c039-58dd-6f4206d1403f>
2011-02-24 23:47:11 +0000

    attempt at inheritence -> its buggy... old code still works

---
+++ Networks/Node.h
@@ -0,0 +1,32 @@
+/* 
+ * File:   Node.h
+ * Author: GENACJE
+ *
+ * Created on February 24, 2011, 4:25 PM
+ */
+
+#ifndef _NODE_H
+#define	_NODE_H
+
+#define KEYSIZE 56 // max blowfish pw length
+
+#include "Connector.h"
+
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    Node(Connector);
+    virtual ~Node();
+
+private:
+    Connector c;
+    
+    char* getStr(char* prompt);
+    bool validate(int mine, int theirs);
+
+};
+
+#endif	/* _NODE_H */
+
