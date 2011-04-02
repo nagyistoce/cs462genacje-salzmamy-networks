220d6efe3a8901dd3f27f2d80c70b2bab624b804
deuchebaggalo@gmail.com <deuchebaggalo@gmail.com@79cf5116-dcd4-c039-58dd-6f4206d1403f>
2011-04-02 20:57:40 +0000

    formatting main

--- trunk/Networks/main.cpp
+++ trunk/Networks/main.cpp
@@ -19,125 +19,83 @@
 #define CLARK "clark.cs.uwec.edu"
 #define SHIVA "shiva.cs.uwec.edu"
 
-char ask_role ();
+char ask_role();
 
 using namespace std;
 
-int main (void) {
+int main(void) {
 
-        char role = ask_role();
+    char role = ask_role();
 
-        // example for KDC:
-        // Node kdc = Node_KDC(c);
-        // kdc.listen();
+    // example for KDC:
+    // Node kdc = Node_KDC(c);
+    // kdc.listen();
 
-        Connector *c;
-        
-        
-        if (role == 'k') {
-            c = new Connector(PORT); // listening constructor
-            Node_KDC kdc = Node_KDC(c);
-            
-             
-            kdc.listen();
-                
-        } else if (role == 'i') {
-            // initiator:
-
-            
-            
-            // get KDC URL
-            char kdc[128];
-            memset(kdc, '\0', 128);
-            Node::getStr(kdc, 128, "Enter the KDC URL:");
-            c = new Connector(kdc, PORT);
-
-            Node_Initiator ini = Node_Initiator(c);
-
-            // send request, receive response from kdc
-            ini.sendRequest();
-            ini.getKDCResponse();
-            
-            
-            // set up connection with receiver
-            char recv[128];
-            memset(recv, '\0', 128);
-            Node::getStr(recv, 128, "Enter the receiver URL:");
-            c = new Connector(recv, PORT);
-            ini.newConnector(c);
-
-            // send encrypted ks to reciever
-            ini.sendSessionKey(); // also handles the receiver response
+    Connector *c;
 
 
-            // get file name
-
-            // sendFile
-
-        } else if (role == 'r') {
-            // receiver:
-            c = new Connector(PORT); // listening constructor
-            Node_Receiver rec = Node_Receiver(c);
-            rec.listen();
-            
-            // receive file
-
-        }
-        
+    if (role == 'k') {
+        c = new Connector(PORT); // listening constructor
+        Node_KDC kdc = Node_KDC(c);
 
 
-        if (role == 'l') {
+        kdc.listen();
 
-                
-                c->set_key("31337");
-                while (1) {
-                    c->listen();
-                    char* msg = c->get_msg();
-                    c->send("ack");
-
-                }
-
-        } else if (role == 't') {
-                // hard coded to the listener for now...
-                Connector* c = new Connector(CLARK, PORT);
-                c->set_key("31337");
-                cout << "Talker: Enter messages to encrypt and send...\n"
-                << "Listener set to: " << CLARK << endl;
-
-                
-                char msg[128];
-                bool quit = false;
-                while(!quit) {
-                    // flush input stream
-                    cin.ignore(128, '\n');
-                    // make sure cin gets the whole thing
-                    cin.get(msg, 128);
-
-                    if (msg == "-quit") {
-                        quit = true;
-                        continue;
-                    }
-                    c->send(msg);
-                    c->listen();
-                    cout << c->get_msg() << endl;
-                }
+    } else if (role == 'i') {
+        // initiator:
 
 
-        } else {
-                perror ("incorrect role input");
-                exit (1);
-        } 
+        // get KDC URL
+        char kdc[128];
+        memset(kdc, '\0', 128);
+        Node::getStr(kdc, 128, "Enter the KDC URL:");
+        c = new Connector(kdc, PORT);
 
-        delete c;
+        Node_Initiator ini = Node_Initiator(c);
 
-        return 0;
+        // send request, receive response from kdc
+        ini.sendRequest();
+        ini.getKDCResponse();
+
+
+        // set up connection with receiver
+        char recv[128];
+        memset(recv, '\0', 128);
+        Node::getStr(recv, 128, "Enter the receiver URL:");
+        c = new Connector(recv, PORT);
+        ini.newConnector(c);
+
+        // send encrypted ks to reciever
+        ini.sendSessionKey(); // also handles the receiver response
+
+
+        // get file name
+
+        // sendFile
+
+    } else if (role == 'r') {
+        // receiver:
+        c = new Connector(PORT); // listening constructor
+        Node_Receiver rec = Node_Receiver(c);
+        rec.listen();
+
+        // receive file
+
+    } else {
+        perror("incorrect role input");
+        exit(1);
+    }
+
+    delete c;
+
+    return 0;
 }
 
-char ask_role () {
-        char role;
+char ask_role() {
+    char role;
 
-        cout << "What role are you?  k - KDC : i - Initiator :: l - listener : t - talker\n";
-        cin >> role;
+    cout << "What role are you?  k - KDC : i - Initiator : r - Receiver\n";
+    cin >> role;
 
-        return role;
+    return role;
 }
