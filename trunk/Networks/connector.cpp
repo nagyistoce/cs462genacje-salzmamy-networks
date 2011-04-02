a111f7fc3e3d80ca3aa0425ab8e35f14315bd042
deuchebaggalo@gmail.com <deuchebaggalo@gmail.com@79cf5116-dcd4-c039-58dd-6f4206d1403f>
2011-04-02 20:51:58 +0000

    output changes to connector

--- trunk/Networks/connector.cpp
+++ trunk/Networks/connector.cpp
@@ -5,110 +5,109 @@ Connector :: Connector () {
 }
 
 Connector :: Connector (int port_num) {
-	addr_len = sizeof (struct sockaddr);
-	numbytes = 0;
-	port = port_num;
-	he = NULL;
-	key = new Blowfish ();
-	msg_size = 1024;
-        buf = new char[msg_size];
+    addr_len = sizeof (struct sockaddr);
+    numbytes = 0;
+    port = port_num;
+    he = NULL;
+    key = new Blowfish ();
+    msg_size = 1024;
+    buf = new char[msg_size];
 
-	my_addr.sin_family = AF_INET; // host byte order
-	my_addr.sin_port = htons (port); // short, network byte order
-	my_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
-	memset (& (my_addr.sin_zero), '\0', 8); // zero the rest of the struct
+    my_addr.sin_family = AF_INET; // host byte order
+    my_addr.sin_port = htons (port); // short, network byte order
+    my_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
+    memset (& (my_addr.sin_zero), '\0', 8); // zero the rest of the struct
 
-	if ((sockfd = socket (AF_INET, SOCK_DGRAM, 0)) == -1) {
-		perror ("socket");
-		exit (1);
-	}
+    if ((sockfd = socket (AF_INET, SOCK_DGRAM, 0)) == -1) {
+        cout << "Failed to create socket. Terminating...\n";
+        exit (1);
+    }
 
-	if (bind (sockfd, (struct sockaddr *) & my_addr, sizeof (struct sockaddr)) == -1) {
-		perror ("bind");
-		exit (1);
-	}
+    if (bind (sockfd, (struct sockaddr *) & my_addr, sizeof (struct sockaddr)) == -1) {
+        cout << "Failed to bind socket. Terminating...\n";
+        exit (1);
+    }
 
 }
 
 
 
 Connector :: Connector (char * receiver, int port_num) {
-	numbytes = 0;
-	port = port_num;
-	addr_len = sizeof (struct sockaddr);
-	key = new Blowfish ();
-	msg_size = 1024;
-        buf = new char[msg_size];
-        
-	my_addr.sin_family = AF_INET; // host byte order
-	my_addr.sin_port = htons(port); // short, network byte order
-	my_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
-	memset(&(my_addr.sin_zero), '\0', 8); // zero the rest of the struct
+    numbytes = 0;
+    port = port_num;
+    addr_len = sizeof (struct sockaddr);
+    key = new Blowfish ();
+    msg_size = 1024;
+    buf = new char[msg_size];
 
-	if ((he = gethostbyname (receiver)) == NULL) { // get the host info
-			perror ("gethostbyname");
-			exit(1);
-		}
+    my_addr.sin_family = AF_INET; // host byte order
+    my_addr.sin_port = htons(port); // short, network byte order
+    my_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
+    memset(&(my_addr.sin_zero), '\0', 8); // zero the rest of the struct
 
-	their_addr.sin_family = AF_INET; // host byte order
-	their_addr.sin_port = htons (port); // short, network byte order
-	their_addr.sin_addr = *((struct in_addr *) he -> h_addr);
-	memset (& (their_addr.sin_zero), '\0',  8); // zero the rest of the struct
+    if ((he = gethostbyname (receiver)) == NULL) { // get the host info
+        cout << "Bad host name entered; Terminating..."<<endl;
+        exit(1);
+    }
 
-	if ((sockfd = socket (AF_INET, SOCK_DGRAM, 0)) == -1) {
-		perror ("socket");
-		exit (1);
-	}
+    their_addr.sin_family = AF_INET; // host byte order
+    their_addr.sin_port = htons (port); // short, network byte order
+    their_addr.sin_addr = *((struct in_addr *) he -> h_addr);
+    memset (& (their_addr.sin_zero), '\0',  8); // zero the rest of the struct
+
+    if ((sockfd = socket (AF_INET, SOCK_DGRAM, 0)) == -1) {
+        cout << "Socket creation failed; Terminating..." << endl;
+        exit (1);
+    }
 }
 
 Connector :: ~Connector () {
-	close (sockfd);
-	delete (key);
-        delete [] buf;
+    close (sockfd);
+    delete (key);
+    delete [] buf;
 }
 
 void Connector :: listen () {
 
-	if ((numbytes = recvfrom (sockfd, buf, msg_size, 0, (struct sockaddr *) & their_addr, (socklen_t *) & addr_len)) == -1) {
-		perror ("recvfrom");
-		exit (1);
-	}
+    if ((numbytes = recvfrom (sockfd, buf, msg_size, 0, (struct sockaddr *) & their_addr, (socklen_t *) & addr_len)) == -1) {
+        cout << "Error in listen(): Failed to receive. Terminating...\n";
+        exit (1);
+    }
 
-	cout << "Before decrypting: " << buf << endl;
-	key -> Decrypt((void *) buf, msg_size);
-	cout << "After decrypting: " << buf << endl;
-	buf [numbytes] = '\0';
+    cout << "Before decrypting: " << buf << endl;
+    key -> Decrypt((void *) buf, msg_size);
+    cout << "After decrypting: " << buf << endl;
+    buf [numbytes] = '\0';
 
-	printf ("got packet from %s\n", inet_ntoa (their_addr.sin_addr));
-	printf ("packet is %d bytes long\n", numbytes);
-//	printf ("packet contains \"%s\"\n", buf);
+    printf ("got packet from %s\n", inet_ntoa (their_addr.sin_addr));
+    printf ("packet is %d bytes long\n", numbytes);
 }
 
 void Connector :: send (char * msg) {
-	char msg_copy [msg_size];
-	memcpy (msg_copy, msg, msg_size);
+    char msg_copy [msg_size];
+    memcpy (msg_copy, msg, msg_size);
 
-	cout << "Unencrypted message to send: " << msg_copy << endl;
-	key -> Encrypt((void *) msg_copy, msg_size);
-	cout << "Encrypted message to send: " << msg_copy << endl;
+    cout << "Unencrypted message to send: " << msg_copy << endl;
+    key -> Encrypt((void *) msg_copy, msg_size);
+    cout << "Encrypted message to send: " << msg_copy << endl;
 
-	if ((numbytes = sendto(sockfd, msg_copy, strlen(msg_copy), 0, (struct sockaddr *) & their_addr, sizeof (struct sockaddr))) == -1) {
-		perror("sendto");
-		exit(1);
-	}
+    if ((numbytes = sendto(sockfd, msg_copy, strlen(msg_copy), 0, (struct sockaddr *) & their_addr, sizeof (struct sockaddr))) == -1) {
+        cout << "Error in send(): Failed to send. Terminating...\n";
+        exit(1);
+    }
 }
 
 void Connector :: send_unencrypted (char * msg) {
-	char msg_copy [msg_size];
-	memcpy (msg_copy, msg, msg_size);
+    char msg_copy [msg_size];
+    memcpy (msg_copy, msg, msg_size);
 
-	cout << "Unencrypted message to send: " << msg_copy << endl;
+    cout << "Unencrypted message to send: " << msg_copy << endl;
 
 
-	if ((numbytes = sendto(sockfd, msg_copy, strlen(msg_copy), 0, (struct sockaddr *) & their_addr, sizeof (struct sockaddr))) == -1) {
-		perror("sendto");
-		exit(1);
-	}
+    if ((numbytes = sendto(sockfd, msg_copy, strlen(msg_copy), 0, (struct sockaddr *) & their_addr, sizeof (struct sockaddr))) == -1) {
+        cout << "Error in send_unencrypted(): Failed to send. Terminating...\n";
+        exit(1);
+    }
 }
 
 char * Connector :: get_msg () {
