ee4a2e359ed64d6c0ae9d071ce6f895dc3926926
deuchebaggalo@gmail.com <deuchebaggalo@gmail.com@79cf5116-dcd4-c039-58dd-6f4206d1403f>
2011-04-03 14:53:07 +0000

    readjusted f funciton into the node class

--- trunk/Networks/Node.cpp
+++ trunk/Networks/Node.cpp
@@ -56,3 +56,19 @@ void Node::newConnector(Connector* newC) {
     delete c;
     c = newC;
 }
+
+long Node::f(long nonce) {
+    const long A = 48271;
+    const long M = 2147483647;
+    const long Q = M/A;
+    const long R = M%A;
+
+	static long state = 1;
+	long t = A * (state % Q) - R * (state / Q);
+
+	if (t > 0)
+		state = t;
+	else
+		state = t + M;
+	return (long)(((double) state/M)* nonce);
+}
